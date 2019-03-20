3c8e410724d4f4ec5905d1cb0088f15f7c36c961
KaoruOnuki <kaoru.onuki@gmail.com>
2019-03-20 15:38:22 +0900

    フォーム送信先追加

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,6 +1,6 @@
 class ContactMailer < ApplicationMailer
   def contact_mail(contact)
     @contact = contact
-    mail to: "kaoru.onuki@gmail.com", subject: "【商標登録】調査依頼メール"
+    mail to: "kaoru.onuki@gmail.com, ko3_4j@hotmail.com", subject: "【商標登録】調査依頼メール"
   end
 end
