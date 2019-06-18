d73fd24ae32ba468f64429268e07e93e448d9851
KaoruOnuki <kaoru.onuki@gmail.com>
2019-06-19 08:45:26 +0900

    deployエラー対応

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,6 +1,3 @@
 class Contact < ApplicationRecord
-  # validates :name, presence: true
-  # validates :phone_number, presence: true
-  # validates :agreed_regulation, presence: true
-  # validates :used, presence: true
+
 end
